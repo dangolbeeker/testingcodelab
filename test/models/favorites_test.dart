161b9b6caedc2688ee5a24c4dc4571fc2c02df29
dangolbeeker <scottb305@outlook.com>
2021-02-11 23:10:49 -0500

    favorites test

--- test/models/favorites_test.dart
+++ test/models/favorites_test.dart
@@ -1,23 +1,48 @@
-import 'package:test/test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 import 'package:testingapp/models/favorites.dart';
+import 'package:testingapp/screens/favorites.dart';
+
+//verifies whether an item disappears when the close (remove) button is pressed.
+
+Favorites favoritesList;
+
+Widget createFavoritesScreen() => ChangeNotifierProvider<Favorites>(
+      create: (context) {
+        favoritesList = Favorites();
+        return favoritesList;
+      },
+      child: MaterialApp(
+        home: FavoritesPage(),
+      ),
+    );
+
+void addItems() {
+  for (var i = 0; i < 10; i += 2) {
+    favoritesList.add(i);
+  }
+}
 
 void main() {
-  group('Testing App Provider Tests', () {
-    var favorites = Favorites();
+  group('Favorites Page Widget Tests', () {
+    testWidgets('Test if ListView shows up', (tester) async {
+      await tester.pumpWidget(createFavoritesScreen());
+      addItems();
+      await tester.pumpAndSettle();
+      expect(find.byType(ListView), findsOneWidget);
+    });
 
-    test('A new item should be added', () {
-      var number = 35;
-      favorites.add(number);
-      expect(favorites.items.contains(number), true);
+    testWidgets('Testing Remove Button', (tester) async {
+      await tester.pumpWidget(createFavoritesScreen());
+      addItems();
+      await tester.pumpAndSettle();
+      var totalItems = tester.widgetList(find.byIcon(Icons.close)).length;
+      await tester.tap(find.byIcon(Icons.close).first);
+      await tester.pumpAndSettle();
+      expect(tester.widgetList(find.byIcon(Icons.close)).length,
+          lessThan(totalItems));
+      expect(find.text('Removed from favorites.'), findsOneWidget);
     });
   });
 }
-
-    test('An item should be removed', () {
-      var number = 45;
-      favorites.add(number);
-      expect(favorites.items.contains(number), true);
-      favorites.remove(number);
-      expect(favorites.items.contains(number), false);
-    });
- 
